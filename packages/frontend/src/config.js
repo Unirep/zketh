3e6fa7471236832cb954fee4102f01666f206fa5
Chance <jchancehud@gmail.com>
2023-02-25 20:49:48 -0600

    fix(ci): frontend config

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -3,15 +3,15 @@ import config from '../../../config'
 
 const prod = NODE_ENV === 'production'
 
-const UNIREP_ADDRESS = prod
+const _UNIREP_ADDRESS = prod
   ? '0x7a2088a1bFc9d81c55368AE168C2C02570cB814F'
   : undefined
-const APP_ADDRESS = prod
+const _APP_ADDRESS = prod
   ? '0x09635F643e140090A9A8Dcd712eD6285858ceBef'
   : undefined
 
-export const UNIREP_ADDRESS = config.UNIREP_ADDRESS ?? UNIREP_ADDRESS
-export const APP_ADDRESS = config.APP_ADDRESS ?? APP_ADDRESS
+export const UNIREP_ADDRESS = config.UNIREP_ADDRESS ?? _UNIREP_ADDRESS
+export const APP_ADDRESS = config.APP_ADDRESS ?? _APP_ADDRESS
 export const ETH_PROVIDER_URL = config.ETH_PROVIDER_URL
 
 export const provider = ETH_PROVIDER_URL.startsWith('http')
