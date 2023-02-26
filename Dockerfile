9bac586903b4ff0cb724f9295efb1d8f169a4295
Chance <jchancehud@gmail.com>
2023-02-25 18:21:11 -0600

    chore(ci): build config

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /src
 
 RUN yarn && rm -rf packages/frontend
 
+RUN yarn circuits build
+
 RUN yarn relay keys
 
 RUN echo "module.exports = {}" > config.js
--- packages/relay/scripts/loadKeys.sh
+++ packages/relay/scripts/loadKeys.sh
@@ -2,6 +2,8 @@
 
 rm -rf ./keys
 
-# cp -r ../../node_modules/@unirep/circuits/zksnarkBuild/. ./keys
+cp -r ../../node_modules/@unirep/circuits/zksnarkBuild/. ./keys
 
-cp -r ../circuits/zksnarkBuild ./keys
+cp -r ../circuits/zksnarkBuild/. ./keys
+
+rm -rf ./keys/*.ptau
