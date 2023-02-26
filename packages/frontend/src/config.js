be340a4bbba97502abcfa60c5ad5a0dd9a4a8e4e
Chance <jchancehud@gmail.com>
2023-02-25 20:30:02 -0600

    fix(frontend): config

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -1,19 +1,28 @@
 import { ethers } from 'ethers'
 import config from '../../../config'
-export const UNIREP_ADDRESS = config.UNIREP_ADDRESS
-export const APP_ADDRESS = config.APP_ADDRESS
+
+const prod = NODE_ENV === 'production'
+
+const UNIREP_ADDRESS = prod
+  ? '0x7a2088a1bFc9d81c55368AE168C2C02570cB814F'
+  : undefined
+const APP_ADDRESS = prod
+  ? '0x09635F643e140090A9A8Dcd712eD6285858ceBef'
+  : undefined
+
+export const UNIREP_ADDRESS = config.UNIREP_ADDRESS ?? UNIREP_ADDRESS
+export const APP_ADDRESS = config.APP_ADDRESS ?? APP_ADDRESS
 export const ETH_PROVIDER_URL = config.ETH_PROVIDER_URL
 
 export const provider = ETH_PROVIDER_URL.startsWith('http')
   ? new ethers.providers.JsonRpcProvider(ETH_PROVIDER_URL)
   : new ethers.providers.WebSocketProvider(ETH_PROVIDER_URL)
 
-export const SERVER =
-  NODE_ENV === 'production' ? 'https://relay.zketh.io' : 'http://localhost:8000'
-export const WS_SERVER =
-  NODE_ENV === 'production' ? 'wss://relay.zketh.io/ws' : 'ws://localhost:8001'
-export const KEY_SERVER =
-  NODE_ENV === 'production'
-    ? 'https://relay.zketh.io/build/'
-    : 'http://localhost:8000/build/'
+export const SERVER = prod ? 'https://relay.zketh.io' : 'http://localhost:8000'
+export const WS_SERVER = prod
+  ? 'wss://relay.zketh.io/ws'
+  : 'ws://localhost:8001'
+export const KEY_SERVER = prod
+  ? 'https://relay.zketh.io/build/'
+  : 'http://localhost:8000/build/'
 // export const KEY_SERVER = 'https://keys.unirep.io/2-beta-1/'
