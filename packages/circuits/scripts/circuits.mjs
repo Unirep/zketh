f1c7e5868b0440bc43dd3a04a5ad3521d47e9e4b
Chance <jchancehud@gmail.com>
2023-02-27 10:39:16 -0600

    feat: address membership proof

--- packages/circuits/scripts/circuits.mjs
+++ packages/circuits/scripts/circuits.mjs
@@ -14,4 +14,5 @@ export const ptauName = 'powersOfTau28_hez_final_20.ptau'
 
 export const circuitContents = {
     signupWithAddress: `pragma circom 2.0.0; include "../circuits/signupWithAddress.circom"; \n\ncomponent main { public [ attester_id, epoch, m ] } = SignupWithAddress(${FIELD_COUNT}, ${EPK_R}, 64, 4);`,
+    proveAddress: `pragma circom 2.0.0; include "../circuits/proveAddress.circom"; \n\ncomponent main { public [ attester_id, epoch, sig_data ] } = ProveAddress(${STATE_TREE_DEPTH}, ${EPK_R}, ${FIELD_COUNT}, ${10});`,
 }
