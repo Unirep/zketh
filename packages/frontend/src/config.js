a4d7d73df05be8ea8e6c660146f8e495bc9554af
Chance <jchancehud@gmail.com>
2023-02-28 17:14:27 -0600

    fix(frontend): localhost dev urls [skip ci]

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -20,11 +20,11 @@ export const provider = ETH_PROVIDER_URL.startsWith('http')
 
 export const SERVER = prod
   ? 'https://relay.zketh.io'
-  : 'http://192.168.1.230:8000'
+  : 'http://localhost:8000'
 export const WS_SERVER = prod
   ? 'wss://relay.zketh.io/ws'
-  : 'ws://192.168.1.230:8001'
+  : 'ws://localhost:8001'
 export const KEY_SERVER = prod
   ? 'https://relay.zketh.io/build/'
-  : 'http://192.168.1.230:8000/build/'
+  : 'http://localhost:8000/build/'
 // export const KEY_SERVER = 'https://keys.unirep.io/2-beta-1/'
