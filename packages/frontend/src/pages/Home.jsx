780f889e84b87d5dc3206242064ca93779da86c2
Chance <jchancehud@gmail.com>
2023-02-27 22:25:01 -0600

    feat(frontend): better editor experience

--- packages/frontend/src/pages/Home.jsx
+++ packages/frontend/src/pages/Home.jsx
@@ -11,22 +11,42 @@ export default observer(() => {
   const { user, msg, auth } = React.useContext(state)
 
   const [draft, setDraft] = React.useState('')
+  const [proving, setProving] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
+  const inputRef = React.useRef(null)
 
   return (
     <div className="container">
       <div className="title">public anonymous chat for ens owners</div>
-      <input
-        type="text"
-        value={draft}
-        onKeyPress={async (e) => {
-          if (e.charCode != 13) return
-          await msg.send(draft)
-          setDraft('')
-        }}
-        onChange={(e) => {
-          setDraft(e.target.value)
-        }}
-      />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <input
+          ref={inputRef}
+          type="text"
+          value={draft}
+          disabled={proving}
+          onKeyPress={async (e) => {
+            if (e.charCode != 13) return
+            try {
+              setError(null)
+              setProving(true)
+              await msg.send(draft)
+              setDraft('')
+              setProving(false)
+              setTimeout(() => inputRef.current.focus(), 1)
+            } catch (err) {
+              setProving(false)
+              setError(err.toString())
+            }
+          }}
+          onChange={(e) => {
+            setDraft(e.target.value)
+          }}
+        />
+        <div style={{ width: '4px' }} />
+        {proving ? <div>Proving...</div> : null}
+        {error ? <div style={{ color: 'red' }}>{error}</div> : null}
+      </div>
       {msg.messages.map((m) => (
         <div key={m._id}>{m.text}</div>
       ))}
