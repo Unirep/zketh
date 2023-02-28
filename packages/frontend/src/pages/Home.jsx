9a84a58d8316ae7458274d952411c2d5ca53f5df
Colleen Rose <cjrose59@gmail.com>
2023-02-27 17:07:26 -0700

    add markdown editor

--- packages/frontend/src/pages/Home.jsx
+++ packages/frontend/src/pages/Home.jsx
@@ -1,21 +1,55 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
+import state from '../contexts/state'
 import './home.css'
 import Tooltip from '../components/Tooltip'
 import Button from '../components/Button'
+import MarkdownIt from 'markdown-it'
+import Editor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css'
 
-import state from '../contexts/state'
+const mdParser = new MarkdownIt({
+  breaks: true,
+  html: false,
+  linkify: true,
+})
 
 export default observer(() => {
   const { user, msg } = React.useContext(state)
 
+  const mdEditor = React.useRef(null)
   const [draft, setDraft] = React.useState('')
+
+  const handleClick = () => {
+    if (mdEditor.current) {
+      alert(mdEditor.current.getMdValue())
+    }
+  }
+
+  const handleEditorChange = ({ html, text }) => {
+    const newDraft = text.replace(/\d/g, '')
+    console.log(newDraft)
+    setDraft(newDraft)
+  }
 
   return (
     <div className="container">
       <div className="title">public anonymous chat for ens owners</div>
-      <input
+
+      <Editor
+        ref={mdEditor}
+        style={{ height: '300px' }}
+        value={draft}
+        renderHTML={(text) => mdParser.render(text)}
+        onChange={handleEditorChange}
+      />
+      <button onClick={handleClick}>send message</button>
+      <div>{draft}</div>
+
+      {/* <div className='flex'>
+        <textarea
+        className='draft'
         type="text"
         value={draft}
         onKeyPress={async (e) => {
@@ -26,7 +60,15 @@ export default observer(() => {
         onChange={(e) => {
           setDraft(e.target.value)
         }}
-      />
+        />
+        <div
+          className='draft'
+          dangerouslySetInnerHTML={{
+              __html: msgHtml,
+          }}
+        />
+      </div> */}
+
       {msg.messages.map((m) => (
         <div key={m._id}>{m.text}</div>
       ))}
