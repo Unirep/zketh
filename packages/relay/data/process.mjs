4ef6437418445ca647ba6cfc53d35d250b3a2f40
Chance <jchancehud@gmail.com>
2023-02-28 16:55:08 -0600

    chore(relay): updated trees

--- packages/relay/data/process.mjs
+++ packages/relay/data/process.mjs
@@ -1,13 +1,14 @@
 import fs from 'fs/promises'
 import { IncrementalMerkleTree } from '@unirep/utils'
 
-const _data = await fs.readFile('./ens_owners.json')
-const addresses = JSON.parse(_data.toString())
+const _data = (await fs.readFile('./ens_owners.csv')).toString().split('\n')
+// const addresses = JSON.parse(_data.toString())
 
-const tree = new IncrementalMerkleTree(17)
+const tree = new IncrementalMerkleTree(20)
 
 let x = 0
-for (const address of addresses) {
+for (const address of _data) {
+  if (!address || !address.startsWith('0x')) continue
   if (++x % 1000 === 0) console.log(x)
   tree.insert(BigInt(address))
 }
@@ -21,4 +22,4 @@ const s = JSON.stringify(out, (_, v) =>
   typeof v === 'bigint' ? `0x${v.toString(16)}` : v
 )
 
-await fs.writeFile('./ens_tree.json', s)
+await fs.writeFile('./ens_tree_new.json', s)
