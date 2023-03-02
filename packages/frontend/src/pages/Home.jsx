d0ca9b1ad4da050c5e12b11b125f102eb35c66c6
Colleen Rose <cjrose59@gmail.com>
2023-03-01 17:02:36 -0700

    responsive columns

--- packages/frontend/src/pages/Home.jsx
+++ packages/frontend/src/pages/Home.jsx
@@ -18,7 +18,8 @@ export default observer(() => {
   const transcriptUrl = new URL(`/transcript/${msg.activeChannel}`, SERVER)
   const dummyMsgs = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45,
   ]
 
   return (
@@ -51,7 +52,7 @@ export default observer(() => {
       <div className="message-grid">
         {dummyMsgs.map((m) => (
           <div className="message">
-            <div className="border">{m}</div>
+            <div className="border">{m} - Ceci n’est pas une message.</div>
           </div>
         ))}
       </div >
